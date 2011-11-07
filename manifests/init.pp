a7b0658bffad51cae273792f01239ee2c8a449dc
Tom Barber <tom@analytical-labs.com>
2011-11-07 22:33:02 +0000

    delete duplicate files

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,8 @@ include tomcat
 
     package { 'pentaho':
         ensure => latest,
-        notify  => Service['tomcat7'],
-	require => Package['tomcat7-alabs'],
+       # notify  => Service['tomcat7'],
+       #require => Package['tomcat7-alabs'],
     }
 
 
