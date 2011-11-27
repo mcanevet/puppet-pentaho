38622cf2d36d3f36c9b70b4c49faf4eba4db6ff3
Tom Barber <tom@analytical-labs.com>
2011-11-27 23:07:35 +0000

    fix indentation

--- manifests/classes/pentaho-mysql.pp
+++ manifests/classes/pentaho-mysql.pp
@@ -1,112 +1,112 @@
 class pentaho::mysql {
 
-include mysql::server
+  include mysql::server
 
-mysql::database{"hibernate":
-  ensure   => present
-}
+  mysql::database{"hibernate":
+    ensure   => present
+  }
 
 
-mysql::rights{"hibernate rights":
-  ensure   => present,
-  database => "hibernate",
-  user     => "hibuser",
-  password => "password",
-  require => Mysql::Database["hibernate"],
-}
+  mysql::rights{"hibernate rights":
+    ensure   => present,
+    database => "hibernate",
+    user     => "hibuser",
+    password => "password",
+    require => Mysql::Database["hibernate"],
+  }
 
-file { "/tmp/create_repository_mysql.sql":
+  file { "/tmp/create_repository_mysql.sql":
     mode => 440,
     owner => root,
     group => root,
     source => "puppet:///modules/pentaho/create_repository_mysql.sql",
     require => Mysql::Rights["hibernate rights"],
     notify => Exec["importhibernate"],
-}
+  }
 
-exec { "importhibernate":
-        cwd => "/tmp",
-        command => "mysql -uroot hibernate < /tmp/create_repository_mysql.sql",
-        refreshonly => true,
-        require => File["/tmp/create_repository_mysql.sql"],
-}
+  exec { "importhibernate":
+    cwd => "/tmp",
+    command => "mysql -uroot hibernate < /tmp/create_repository_mysql.sql",
+    refreshonly => true,
+    require => File["/tmp/create_repository_mysql.sql"],
+  }
 
 
-file { "/tmp/create_sample_datasource_mysql.sql":
+  file { "/tmp/create_sample_datasource_mysql.sql":
     mode => 440,
     owner => root,
     group => root,
     source => "puppet:///modules/pentaho/create_sample_datasource_mysql.sql",
     require => Mysql::Rights["hibernate rights"],
     notify => Exec["importhibernate2"],
-}
+  }
 
-exec { "importhibernate2":
-        cwd => "/tmp",
-        command => "mysql -uroot hibernate < /tmp/create_sample_datasource_mysql.sql",
-        refreshonly => true,
-        require => File["/tmp/create_sample_datasource_mysql.sql"],
-}
-
-
-mysql::database{"sampledata":
-  ensure   => present
-}
+  exec { "importhibernate2":
+    cwd => "/tmp",
+    command => "mysql -uroot hibernate < /tmp/create_sample_datasource_mysql.sql",
+    refreshonly => true,
+    require => File["/tmp/create_sample_datasource_mysql.sql"],
+  }
 
 
-mysql::rights{"sampledata rights":
-  ensure   => present,
-  database => "sampledata",
-  user     => "pentaho_user",
-  password => "password",
-  require => Mysql::Database["sampledata"],
-}
+  mysql::database{"sampledata":
+    ensure   => present
+  }
 
-file { "/tmp/sampledata_mysql.sql":
+
+  mysql::rights{"sampledata rights":
+    ensure   => present,
+    database => "sampledata",
+    user     => "pentaho_user",
+    password => "password",
+    require => Mysql::Database["sampledata"],
+  }
+
+  file { "/tmp/sampledata_mysql.sql":
     mode => 440,
     owner => root,
     group => root,
     source => "puppet:///modules/pentaho/sampledata_mysql.sql",
     require => Mysql::Rights["sampledata rights"],
     notify => Exec["importsampledata"],
-}
+  }
 
-exec { "importsampledata":
-        cwd => "/tmp",
-        command => "mysql -uroot sampledata < /tmp/sampledata_mysql.sql",
-        refreshonly => true,
-        require => File["/tmp/sampledata_mysql.sql"],
-}
-
-
-mysql::database{"quartz":
-  ensure   => present
-}
+  exec { "importsampledata":
+    cwd => "/tmp",
+    command => "mysql -uroot sampledata < /tmp/sampledata_mysql.sql",
+    refreshonly => true,
+    require => File["/tmp/sampledata_mysql.sql"],
+  }
 
 
-mysql::rights{"quartz rights":
-  ensure   => present,
-  database => "quartz",
-  user     => "pentaho_user",
-  password => "password",
-  require => Mysql::Database["quartz"],
-}
+  mysql::database{"quartz":
+    ensure   => present
+  }
 
-file { "/tmp/create_quartz_mysql.sql":
+
+  mysql::rights{"quartz rights":
+    ensure   => present,
+    database => "quartz",
+    user     => "pentaho_user",
+    password => "password",
+    require => Mysql::Database["quartz"],
+  }
+
+  file { "/tmp/create_quartz_mysql.sql":
     mode => 440,
     owner => root,
     group => root,
     source => "puppet:///modules/pentaho/create_quartz_mysql.sql",
     require => Mysql::Rights["quartz rights"],
     notify => Exec["importquartz"],
-}
+  }
 
-exec { "importquartz":
-        cwd => "/tmp",
-        command => "mysql -uroot quartz < /tmp/create_quartz_mysql.sql",
-        refreshonly => true,
-        require => File["/tmp/create_quartz_mysql.sql"],
-}
+  exec { "importquartz":
+    cwd => "/tmp",
+    command => "mysql -uroot quartz < /tmp/create_quartz_mysql.sql",
+    refreshonly => true,
+    require => File["/tmp/create_quartz_mysql.sql"],
+  }
 
 
 
--- manifests/definitions/pentahoinstance.pp
+++ manifests/definitions/pentahoinstance.pp
@@ -23,86 +23,86 @@ define pentaho::biserver::instance($ensure , $tomcat_http, $tomcat_ajp, $tomcat_
   include tomcat::source
 
   package { "pentaho-biserver":
-     ensure => present,
-     require => Tomcat::Instance["pentaho_biserver"],
-     }
-     
+    ensure => present,
+    require => Tomcat::Instance["pentaho_biserver"],
+  }
+
   tomcat::instance {"pentaho_biserver":
     ensure      => present,
     ajp_port    => "${tomcat_ajp}",
     server_port    => "${tomcat_server}",
     http_port    => "${tomcat_http}",
   }
-  
+
   class { "pentaho::mysql":
     require => Package["pentaho-biserver"],
   }
   #pentaho/META-INF/context.xml
-    file { '/srv/tomcat/pentaho_biserver/webapps/pentaho/META-INF/context.xml':
-      ensure  => present,
-      content => template('pentaho/pentaho_context.xml.erb'),
-      mode    => 755,
-      require => Package["pentaho-biserver"],
-      }
-    
-      file { ["/srv/tomcat/pentaho_biserver/conf/",
-        "/srv/tomcat/pentaho_biserver/conf/Catalina/",
-        "/srv/tomcat/pentaho_biserver/conf/Catalina/localhost/"]:
-            ensure  => "directory",
-            mode    => 755,
-            require => Package["pentaho-biserver"],
-            }
-    #pentaho/META-INF/context.xml
-      file { '/srv/tomcat/pentaho_biserver/conf/Catalina/localhost/pentaho.xml':
-        ensure  => present,
-        content => template('pentaho/pentaho_context.xml.erb'),
-        mode    => 755,
-        require => [Package["pentaho-biserver"], File["/srv/tomcat/pentaho_biserver/conf/",
-        "/srv/tomcat/pentaho_biserver/conf/Catalina/",
-        "/srv/tomcat/pentaho_biserver/conf/Catalina/localhost/"]],
-        }
-#pentaho/WEB-INF/web.xml
-    file { '/srv/tomcat/pentaho_biserver/webapps/pentaho/WEB-INF/web.xml':
+  file { '/srv/tomcat/pentaho_biserver/webapps/pentaho/META-INF/context.xml':
+    ensure  => present,
+    content => template('pentaho/pentaho_context.xml.erb'),
+    mode    => 755,
+    require => Package["pentaho-biserver"],
+  }
+
+  file { ["/srv/tomcat/pentaho_biserver/conf/",
+  "/srv/tomcat/pentaho_biserver/conf/Catalina/",
+  "/srv/tomcat/pentaho_biserver/conf/Catalina/localhost/"]:
+    ensure  => "directory",
+    mode    => 755,
+    require => Package["pentaho-biserver"],
+  }
+  #pentaho/META-INF/context.xml
+  file { '/srv/tomcat/pentaho_biserver/conf/Catalina/localhost/pentaho.xml':
+    ensure  => present,
+    content => template('pentaho/pentaho_context.xml.erb'),
+    mode    => 755,
+    require => [Package["pentaho-biserver"], File["/srv/tomcat/pentaho_biserver/conf/",
+    "/srv/tomcat/pentaho_biserver/conf/Catalina/",
+    "/srv/tomcat/pentaho_biserver/conf/Catalina/localhost/"]],
+  }
+  #pentaho/WEB-INF/web.xml
+  file { '/srv/tomcat/pentaho_biserver/webapps/pentaho/WEB-INF/web.xml':
     ensure  => present,
     content => template('pentaho/pentaho_web.xml.erb'),
     mode    => 755,
-      require => Package["pentaho-biserver"],
-    }
+    require => Package["pentaho-biserver"],
+  }
 
-#pentaho-solutions/system/applicationContext-spring-security-hibernate.properties
-    file { '/opt/pentaho-solutions/system/applicationContext-spring-security-hibernate.properties':
+  #pentaho-solutions/system/applicationContext-spring-security-hibernate.properties
+  file { '/opt/pentaho-solutions/system/applicationContext-spring-security-hibernate.properties':
     ensure  => present,
     content => template('pentaho/solution_applicationContext-spring-security-hibernate.properties.erb'),
     mode    => 755,
-      require => Package["pentaho-biserver"],
-    }
+    require => Package["pentaho-biserver"],
+  }
 
-#pentaho-solutions/system/hibernate/mysql5.hibernate.cfg.xml
-    file { '/opt/pentaho-solutions/system/hibernate/mysql5.hibernate.cfg.xml':
+  #pentaho-solutions/system/hibernate/mysql5.hibernate.cfg.xml
+  file { '/opt/pentaho-solutions/system/hibernate/mysql5.hibernate.cfg.xml':
     ensure  => present,
     content => template('pentaho/solution_mysql5.hibernate.cfg.xml.erb'),
     mode    => 755,
-      require => Package["pentaho-biserver"],
-    }
+    require => Package["pentaho-biserver"],
+  }
 
   file { '/srv/tomcat/pentaho_biserver/lib/mysql-connector-java-5.1.17.jar':
-      ensure  => present,
-      source => "puppet:///modules/pentaho/mysql-connector-java-5.1.17.jar",
-      mode    => 755,
-        require => Package["pentaho-biserver"],
-      }
+    ensure  => present,
+    source => "puppet:///modules/pentaho/mysql-connector-java-5.1.17.jar",
+    mode    => 755,
+    require => Package["pentaho-biserver"],
+  }
 
-      file { '/opt/pentaho-solutions/system/hibernate/hibernate-settings.xml':
-      ensure  => present,
-      content => template('pentaho/solution_hibernate-settings.xml.erb'),
-      mode    => 755,
-        require => Package["pentaho-biserver"],
-      }
-      
+  file { '/opt/pentaho-solutions/system/hibernate/hibernate-settings.xml':
+    ensure  => present,
+    content => template('pentaho/solution_hibernate-settings.xml.erb'),
+    mode    => 755,
+    require => Package["pentaho-biserver"],
+  }
+
   file { '/srv/tomcat/pentaho_biserver/webapps/pentaho/WEB-INF/classes/log4j.xml':
     ensure  => present,
     content => template('pentaho/pentaho_log4j.xml.erb'),
     mode    => 755,
     require => Package["pentaho-biserver"],
-    }
+  }
 }
