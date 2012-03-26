71245e797ecc3f1cffed71a4c70b76efff2854c1
Tom Barber <tom@analytical-labs.com>
2012-03-26 16:34:07 +0100

    fix control

--- manifests/biserver/instance.pp
+++ manifests/biserver/instance.pp
@@ -18,31 +18,38 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301  USA
 #
-define pentaho::biserver::instance($ensure , $tomcat_http, $tomcat_ajp, $tomcat_server, $database) {
-  $tomcat_version = "6.0.29"
-  include tomcat::source
-  
- 
-  tomcat::instance {"pentaho_biserver":
-    ensure      => present,
-    ajp_port    => "${tomcat_ajp}",
-    server_port    => "${tomcat_server}",
-    http_port    => "${tomcat_http}",
-    require => Class["pentaho::mysql"],
-  }
-  
-
-	if($database=="mysql"){
-		include pentaho::mysql
-   class { "pentaho::server":
-   	require => Class["pentaho::mysql"],
-   	}
-   	}
-   	
-   	if($database=="postgresql8"){
-   		include pentaho::postgresql
-   	 class { "pentaho::server":
-   	require => Class["Pentaho::Postgresql"],
-   	}
-   	}	
+define pentaho::biserver::instance ($ensure,
+	$tomcat_http,
+	$tomcat_ajp,
+	$tomcat_server,
+	$database) {
+	$tomcat_version = "6.0.29"
+	include tomcat::source if ($database == "mysql") {
+		include pentaho::mysql class {
+			"pentaho::server" :
+				require => Class["pentaho::mysql"],
+		}
+		tomcat::instance {
+			"pentaho_biserver" :
+				ensure => present,
+				ajp_port => "${tomcat_ajp}",
+				server_port => "${tomcat_server}",
+				http_port => "${tomcat_http}",
+				require => Class["pentaho::mysql"],
+		}
+	}
+	if ($database == "postgresql8") {
+		include pentaho::postgresql class {
+			"pentaho::server" :
+				require => Class["Pentaho::Postgresql"],
+		}
+		tomcat::instance {
+			"pentaho_biserver" :
+				ensure => present,
+				ajp_port => "${tomcat_ajp}",
+				server_port => "${tomcat_server}",
+				http_port => "${tomcat_http}",
+				require => Class["pentaho::postgresl"],
+		}
+	}
 }
