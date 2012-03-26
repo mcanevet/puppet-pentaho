dafbd4300eeb5c41a39dd4dee0d129cc109bf23f
Tom Barber <tom@analytical-labs.com>
2012-03-26 17:35:22 +0100

    new postgres files

--- manifests/postgresql.pp
+++ manifests/postgresql.pp
@@ -1,72 +1,72 @@
 class pentaho::postgresql {
-#	mysql::database {
-#		"hibernate" :
-#			ensure => present
-#	}
-#	mysql::rights {
-#		"hibernate rights" :
-#			ensure => present,
-#			database => "hibernate",
-#			user => "hibuser",
-#			password => "password",
-#			require => Mysql::Database["hibernate"],
-#	}
-#	file {
-#		"/srv/pentahodata/create_repository_mysql.sql" :
-#			mode => 440,
-#			owner => root,
-#			group => root,
-#			source => "puppet:///modules/pentaho/create_repository_mysql.sql",
-#			require => Mysql::Rights["hibernate rights"],
-#			notify => Exec["importhibernate"],
-#	}
-#	exec {
-#		"importhibernate" :
-#			cwd => "/tmp",
-#			command =>
-#			"mysql -uroot hibernate < /srv/pentahodata/create_repository_mysql.sql",
-#			refreshonly => true,
-#			require => File["/srv/pentahodata/create_repository_mysql.sql"],
-#	}
-#	file {
-#		"/srv/pentahodata/create_sample_datasource_mysql.sql" :
-#			mode => 440,
-#			owner => root,
-#			group => root,
-#			source => "puppet:///modules/pentaho/create_sample_datasource_mysql.sql",
-#			require => Mysql::Rights["hibernate rights"],
-#			notify => Exec["importhibernate2"],
-#	}
-#	exec {
-#		"importhibernate2" :
-#			cwd => "/tmp",
-#			command =>
-#			"mysql -uroot hibernate < /srv/pentahodata/create_sample_datasource_mysql.sql",
-#			refreshonly => true,
-#			require => [File["/srv/pentahodata/create_sample_datasource_mysql.sql"], Exec["importhibernate"]],
-#	}
-#	mysql::database {
-#		"sampledata" :
-#			ensure => present
-#	}
-#	mysql::rights {
-#		"sampledata rights" :
-#			ensure => present,
-#			database => "sampledata",
-#			user => "pentaho_user",
-#			password => "password",
-#			require => Mysql::Database["sampledata"],
-#	}
-#	file {
-#		"/srv/pentahodata/sampledata_mysql.sql" :
-#			mode => 440,
-#			owner => root,
-#			group => root,
-#			source => "puppet:///modules/pentaho/sampledata_mysql.sql",
-#			require => Mysql::Rights["sampledata rights"],
-#			notify => Exec["importsampledata"],
-#	}
 
+  file { "/srv/pentahodata/create_repository_postgresql.sql.gz":
+    mode => 750,
+    owner => postgres,
+    group => postgres,
+    source => "puppet:///modules/pentaho/create_repository_postgresql.sql.gz",
+    require => File["/srv/pentahodata"],
+    notify => Postgresql::Database["hibernate"],
+  }
+  
+  postgresql::database{ "hibernate":
+  ensure=>present,
+  owner=>pentaho_user,
+  encoding=>"UTF8",
+  template=>"template1",
+  source=>"/srv/pentahodata/create_repository_postgresql.sql.gz",
+  overwrite=>false
+  }
+  
+  
+    file { "/srv/pentahodata/create_quartz_postgresql.sql.gz":
+    mode => 750,
+    owner => postgres,
+    group => postgres,
+    source => "puppet:///modules/pentaho/create_quartz_postgresql.sql.gz",
+    require => File["/srv/pentahodata"],
+    notify => Postgresql::Database["quartz"],
+  }
+  
+  postgresql::database{ "quartz":
+  ensure=>present,
+  owner=>pentaho_user,
+  encoding=>"UTF8",
+  template=>"template1",
+  source=>"/srv/pentahodata/sampledata_postgresql.sql.gz",
+  overwrite=>false
+  }
+  
+  
+    file { "/srv/pentahodata/create_sample_datasource_postgresql.sql.gz":
+    mode => 750,
+    owner => postgres,
+    group => postgres,
+    source => "puppet:///modules/pentaho/create_sample_datasource_postgresql.sql.gz",
+    require => File["/srv/pentahodata"],
+  }
+ 
+ exec { "Import dump into create datasource postgres db":
+      command => "zcat /srv/pentahodata/create_sample_datasource_postgresql.sql.gz | psql hibernate",
+      user => "postgres",
+      require => File["/srv/pentahodata/create_sample_datasource_postgresql.sql.gz"],
+    }
+  
+  
+    file { "/srv/pentahodata/load_sample_users_postgresql.sql.gz":
+    mode => 750,
+    owner => postgres,
+    group => postgres,
+    source => "puppet:///modules/pentaho/load_sample_users_postgresql.sql.gz",
+    require => File["/srv/pentahodata"],
+  }
+  exec { "Import dump into load sample users postgres db":
+      command => "zcat /srv/pentahodata/load_sample_users_postgresql.sql.gz | psql quartz",
+      user => "postgres",
+      require =>File["/srv/pentahodata/load_sample_users_postgresql.sql.gz"],
+    }
+  
+  
   file { "/srv/pentahodata/sampledata_postgresql.sql.gz":
     mode => 750,
     owner => postgres,
@@ -84,40 +84,5 @@ postgresql::database{ "sampledata":
   overwrite=>false
   }
    
-#	exec {
-#		"importsampledata" :
-#			cwd => "/tmp",
-#			command =>
-#			"mysql -uroot sampledata < /srv/pentahodata/sampledata_mysql.sql",
-#			refreshonly => true,
-#			require => File["/srv/pentahodata/sampledata_mysql.sql"],
-#	}
-#	mysql::database {
-#		"quartz" :
-#			ensure => present
-#	}
-#	mysql::rights {
-#		"quartz rights" :
-#			ensure => present,
-#			database => "quartz",
-#			user => "pentaho_user",
-#			password => "password",
-#			require => Mysql::Database["quartz"],
-#	}
-#	file {
-#		"/srv/pentahodata/create_quartz_mysql.sql" :
-#			mode => 440,
-#			owner => root,
-#			group => root,
-#			source => "puppet:///modules/pentaho/create_quartz_mysql.sql",
-#			require => Mysql::Rights["quartz rights"],
-#			notify => Exec["importquartz"],
-#	}
-#	exec {
-#		"importquartz" :
-#			cwd => "/tmp",
-#			command => "mysql -uroot quartz < /srv/pentahodata/create_quartz_mysql.sql",
-#			refreshonly => true,
-#			require => File["/srv/pentahodata/create_quartz_mysql.sql"],
-#	}
+
 }
